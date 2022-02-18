81d9440ae9ca0515a21a64614d1701e4e72ee33a
Dmitry Marinin <436910463@mail.ru>
2022-02-18 14:01:31 +0300

    update link to fork fixes

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!))
+        .package(name: "RealmDatabase", url: "https://github.com/YaphetS1/realm-core", .branch("fix/issueLinkSecure")))
     ],
     targets: [
       .target(
